053a4b9ec60485996d7e6730f9b094a2df14bf7a
RISHI <dileep556723@gmail.com>
2026-05-18 19:04:57 +0530

    Completed FAQ and website updates

--- src/App.jsx
+++ src/App.jsx
@@ -381,7 +381,8 @@ return (
               Nurturing Excellence Through Education
             </h3>
                <div className="mb-2 text-6xl font-bold leading-none text-[#c9a227]/30">
-                 “</div>    
+                 “
+               </div>    
               <p className="mt-2 leading-8 text-slate-700">
               At SHIVA SAI SCHOOL, we believe education is the foundation for a
               successful and responsible future. Our mission is to create a
